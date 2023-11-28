13d2cf66911d8e35009011b0ebd7f554114191da
eloranta <hannu.eloranta@gmail.com>
2023-11-28 19:39:55 +0200

    add: more tests

---
+++ test/locator.test.ts
@@ -0,0 +1,28 @@
+import { locator } from "../src/locator";
+
+describe("locator function", () => {
+  it("should return 'AJ00AA'", () => {
+    expect(locator({longitude: -180.0, latitude: 0.0})).toEqual("AJ00AA")
+  })
+})
+describe("locator function", () => {
+  it("should return 'EJ50AA'", () => {
+    expect(locator({longitude: -90.0, latitude: 0.0})).toEqual("EJ50AA")
+  })
+})
+describe("locator function", () => {
+  it("should return 'JJ00AA'", () => {
+    expect(locator({longitude: 0.0, latitude: 0.0})).toEqual("JJ00AA")
+  })
+})
+describe("locator function", () => {
+  it("should return 'NJ50AA'", () => {
+    expect(locator({longitude: 90.0, latitude: 0.0})).toEqual("NJ50AA")
+  })
+})
+describe("locator function", () => {
+  it("should return 'JJ00AA'", () => {
+    expect(locator({longitude: 180.0, latitude: 0.0})).toEqual("AJ00AA")
+  })
+})
+
--- test/math.test.ts
+++
@@ -1,8 +0,0 @@
-import { locator } from "../src/locator";
-
-describe("locator function", () => {
-  it("should return 'JJ00AA'", () => {
-    const result = locator({longitude: 0.0, latitude: 0.0})
-    expect(result).toEqual("JJ00AA")
-  })
-})
